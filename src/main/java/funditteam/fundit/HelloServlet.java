2ad55c99588282642d1ea8a9a20ecdafce62f6a1
luigi-crescenzo7 <luigi.crescenzo.7@hotmail.it>
2022-01-03 19:11:11 +0100

    first test with checkstyle

--- src/main/java/funditteam/fundit/HelloServlet.java
+++ src/main/java/funditteam/fundit/HelloServlet.java
@@ -1,31 +1,45 @@
 package funditteam.fundit;
 
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet
-{
-   private String message;
+public class HelloServlet extends HttpServlet {
+    /**
+     * @
+     */
+    private String message;
 
-   public void init()
-   {
-      message = "Hello Fund.It team";
-   }
+    /**
+     *
+     */
+    public void init() {
+        message = "Hello Fund.It team";
+    }
 
-   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
-   {
-      response.setContentType("text/html");
+    /**
+     * @param request
+     * @param response
+     */
+    public void doGet(final HttpServletRequest request,
+                      final HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
 
-      // Hello
-      PrintWriter out = response.getWriter();
-      out.println("<html><body>");
-      out.println("<h1>" + message + "</h1>");
-      out.println("</body></html>");
-   }
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
 
-   public void destroy()
-   {
-   }
+    /**
+     *
+     */
+    public void destroy() {
+    }
 }
