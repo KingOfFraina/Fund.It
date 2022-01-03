b2bf06fe9ca48e1dfcd70447f24738392df18d16
Sabato Genovese <sabatinogenovese51@gmail.com>
2022-01-03 18:30:57 +0100

    Test Commit

--- src/main/java/funditteam/fundit/HelloServlet.java
+++ src/main/java/funditteam/fundit/HelloServlet.java
@@ -11,7 +11,7 @@ public class HelloServlet extends HttpServlet
 
    public void init()
    {
-      message = "Hello World!";
+      message = "Hello Fund.It team";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
