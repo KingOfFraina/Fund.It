0b8b6a64fcef17f0f900b0fc0d6b9ab0846c10af
Sabato Genovese <48244524+SabatoGenovese@users.noreply.github.com>
2022-01-04 16:45:00 +0100

    Correzione ConPool.java

--- src/main/java/controller/Servlet.java
+++ src/main/java/controller/Servlet.java
@@ -1,28 +1,32 @@
 package controller;
 
 import model.persistence.ConPool;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @WebServlet(name = "Servlet", value = "/Servlet")
-public class Servlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            Connection c = ConPool.getConnection();
-            System.out.println();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+public class Servlet extends HttpServlet
+{
+   @Override
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+   {
+      try
+      {
+         Connection c = ConPool.getInstance().getConnection();
+         PreparedStatement ps = c.prepareStatement("SELECT * FROM utente");
+         ResultSet rs = ps.executeQuery();
+         rs.next();
+         System.out.println(rs.getString("nome") + rs.getString("cognome"));
+      }
+      catch (SQLException e)
+      {
+         e.printStackTrace();
+      }
+   }
 }
--- src/main/java/model/persistence/ConPool.java
+++ src/main/java/model/persistence/ConPool.java
@@ -1,65 +1,45 @@
-/*
-BSD 3-Clause License
-
-Copyright (c) 2019, Mattia De Rosa
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* Neither the name of the copyright holder nor the names of its
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 package model.persistence;
-
 
 import org.apache.tomcat.jdbc.pool.PoolProperties;
 import org.apache.tomcat.jdbc.pool.DataSource;
-
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.TimeZone;
 
-public class ConPool {
-    private static DataSource datasource;
+public class ConPool
+{
+   private DataSource datasource;
+   private static ConPool instance;
 
+   private ConPool()
+   {
+   }
 
-    public static Connection getConnection() throws SQLException {
-        if (datasource == null) {
+   public static ConPool getInstance()
+   {
+      if(instance == null)
+         instance = new ConPool();
 
-            PoolProperties p = new PoolProperties();
-            p.setUrl("jdbc:mysql://fundit-mysqlserver.mysql.database.azure.com:3306/funditdb?useSSL=true");
-            p.setDriverClassName("com.mysql.cj.jdbc.Driver");
-            p.setUsername("funditadmin");
-            System.out.println(System.getenv("WE"));
-            p.setPassword("cAEmiz5Dx3PZRQ");
-            p.setMaxActive(100);
-            p.setInitialSize(10);
-            p.setMinIdle(10);
-            p.setRemoveAbandonedTimeout(60);
-            p.setRemoveAbandoned(true);
-            datasource = new DataSource();
-            datasource.setPoolProperties(p);
-        }
-        return datasource.getConnection();
-    }
+      return instance;
+   }
+
+   public Connection getConnection() throws SQLException
+   {
+      if (datasource == null)
+      {
+         PoolProperties p = new PoolProperties();
+         p.setUrl("jdbc:mysql://fundit-mysqlserver.mysql.database.azure.com:3306/funditdb?useSSL=true");
+         p.setDriverClassName("com.mysql.cj.jdbc.Driver");
+         p.setUsername("funditadmin");
+         p.setPassword("cAEmiz5Dx3PZRQ");
+         p.setMaxActive(100);
+         p.setInitialSize(10);
+         p.setMinIdle(10);
+         p.setRemoveAbandonedTimeout(60);
+         p.setRemoveAbandoned(true);
+         datasource = new DataSource();
+         datasource.setPoolProperties(p);
+      }
+
+      return datasource.getConnection();
+   }
 }
