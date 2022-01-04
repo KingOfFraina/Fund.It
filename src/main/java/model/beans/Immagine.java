b5365052c29906d4d37b9c468ad4f3a341f418c0
Francesco <francesco.expo00@gmail.com>
2022-01-04 17:39:01 +0100

    added Immagine.java

---
+++ src/main/java/model/beans/Immagine.java
@@ -0,0 +1,44 @@
+package model.beans;
+
+public final class Immagine {
+    /**
+     * Riferimento alla campagna.
+     */
+    private Campagna campagna;
+    /**
+     * Path immagine.
+     */
+    private String path;
+
+    /**
+     * Getter Campagna.
+     * @return la campagna relativa
+     */
+    public Campagna getCampagna() {
+        return campagna;
+    }
+
+    /**
+     * Getter Path.
+     * @return la stringa del path
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * Setter Campagna.
+     * @param newCampagna riferimento alla campagna
+     */
+    public void setCampagna(final Campagna newCampagna) {
+        this.campagna = newCampagna;
+    }
+
+    /**
+     * Setter path.
+     * @param imgPath specifica il path dell'immagine
+     */
+    public void setPath(final String imgPath) {
+        this.path = imgPath;
+    }
+}
