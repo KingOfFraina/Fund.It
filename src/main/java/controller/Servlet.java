df0d089b8168c8e2919f2e32fcee10989b3b6a43
luigi-crescenzo7 <luigi.crescenzo.7@hotmail.it>
2022-01-04 16:18:01 +0100

    added ConPool.java

---
+++ src/main/java/controller/Servlet.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.persistence.ConPool;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet(name = "Servlet", value = "/Servlet")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Connection c = ConPool.getConnection();
+            System.out.println();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/model/beans/TestJdb.java
@@ -0,0 +1,15 @@
+package model.beans;
+
+import model.persistence.ConPool;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestJdb {
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
