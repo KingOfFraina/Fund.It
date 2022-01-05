f930dcb3bd7923f742979b6922dd11945a1e600e
Sabato Genovese <48244524+SabatoGenovese@users.noreply.github.com>
2022-01-05 19:08:24 +0100

    Fix interfaccia DAO.java

--- src/main/java/model/DAO/DAO.java
+++ src/main/java/model/DAO/DAO.java
@@ -1,15 +1,21 @@
 package model.DAO;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface DAO<T> {
-   T getById(int id);
+   /**
+    * @param id rappresenta l'identificativo dell'entity
+    * @return null se non viene trovato nessun risultato,
+    * un'instanza nel caso in cui viene trovato un risultato
+    */
+   T getById(int id) throws SQLException;
 
-   List<T> getAll();
+   List<T> getAll() throws SQLException;
 
-   boolean save(T entity);
+   boolean save(T entity) throws SQLException;
 
-   boolean update(T entity);
+   boolean update(T entity) throws SQLException;
 
-   boolean delete(T entity);
+   boolean delete(T entity) throws SQLException;
 }
