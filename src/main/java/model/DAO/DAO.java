51ef69dd73493696b0a25727948c282de0e2f748
Sabato Genovese <48244524+SabatoGenovese@users.noreply.github.com>
2022-01-05 19:22:51 +0100

    Completata interfaccia DAO.java

--- src/main/java/model/DAO/DAO.java
+++ src/main/java/model/DAO/DAO.java
@@ -7,7 +7,7 @@ public interface DAO<T> {
    /**
     * @param id rappresenta l'identificativo dell'entity
     * @return null se non viene trovato nessun risultato,
-    * un'instanza di T nel caso in cui viene trovato un risultato
+    * un'istanza di T nel caso in cui viene trovato un risultato
     */
    T getById(int id);
 
@@ -16,11 +16,32 @@ public interface DAO<T> {
     */
    List<T> getAll();
 
+   /**
+    * @param entity l'istanza da salvare
+    * @return false --> se l'operazione non va a buon fine,
+    * true --> se l'operazione va a buon fine
+    */
    boolean save(T entity);
 
+   /**
+    * @param entity l'istanza da aggiornare
+    * @return false --> se l'operazione non va a buon fine,
+    * true --> se l'operazione va a buon fine
+    */
    boolean update(T entity);
 
+   /**
+    * @param entity l'istanza da eliminare
+    * @return false --> se l'operazione non va a buon fine,
+    * true --> se l'operazione va a buon fine
+    */
    boolean delete(T entity);
 
-   T extractor(ResultSet resultSet, String alias);
+   /**
+    * @param resultSet resultSet della query eseguita
+    * @param alias eventuale alias del field
+    * @return l'istanza della della classe T popolata con le informazioni
+    * presenti nel resultSet
+    */
+   T extract(ResultSet resultSet, String alias);
 }
