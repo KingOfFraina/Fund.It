9f026653f4c845e6f52029a477052a7516d7461d
Sabato Genovese <48244524+SabatoGenovese@users.noreply.github.com>
2022-01-17 20:06:37 +0100

    other changes by luigi

--- src/main/java/model/beans/Utente.java
+++ src/main/java/model/beans/Utente.java
@@ -71,220 +71,221 @@ public final class Utente implements UtenteInterface {
     */
    private List<Segnalazione> segnalazioni;
 
-    /**
-     * Lista delle campagne avviate dall'utente.
-     */
-    private List<Campagna> campagnaList;
+   /**
+    * Lista delle campagne avviate dall'utente.
+    */
+   private List<Campagna> campagne;
 
-    /**
-     * @return idUtente.
-     */
-    public int getIdUtente() {
-        return idUtente;
-    }
+   /**
+    * @return idUtente.
+    */
+   public int getIdUtente() {
+      return idUtente;
+   }
 
-    /**
-     * @param id idUtente.
-     */
-    public void setIdUtente(final int id) {
-        this.idUtente = id;
-    }
+   /**
+    * @param id idUtente.
+    */
+   public void setIdUtente(final int id) {
+      this.idUtente = id;
+   }
 
-    /**
-     * @return dataBan.
-     */
-    public Date getDataBan() {
-        return dataBan;
-    }
+   /**
+    * @return dataBan.
+    */
+   public Date getDataBan() {
+      return dataBan;
+   }
 
-    /**
-     * @param data Data del ban.
-     */
-    public void setDataBan(final Date data) {
-        this.dataBan = data;
-    }
+   /**
+    * @param data Data del ban.
+    */
+   public void setDataBan(final Date data) {
+      this.dataBan = data;
+   }
 
-    /**
-     * @return isAdmin.
-     */
-    public boolean isAdmin() {
-        return admin;
-    }
+   /**
+    * @return isAdmin.
+    */
+   public boolean isAdmin() {
+      return admin;
+   }
 
-    /**
-     * @param adm bool (Admin==1/Utente normale==0).
-     */
-    public void setAdmin(final boolean adm) {
-        this.admin = adm;
-    }
+   /**
+    * @param adm bool (Admin==1/Utente normale==0).
+    */
+   public void setAdmin(final boolean adm) {
+      this.admin = adm;
+   }
 
-    /**
-     * @return Path della foto profilo.
-     */
-    public String getFotoProfilo() {
-        return fotoProfilo;
-    }
+   /**
+    * @return Path della foto profilo.
+    */
+   public String getFotoProfilo() {
+      return fotoProfilo;
+   }
 
-    /**
-     * @param path Path della foto profilo.
-     */
-    public void setFotoProfilo(final String path) {
-        this.fotoProfilo = path;
-    }
+   /**
+    * @param path Path della foto profilo.
+    */
+   public void setFotoProfilo(final String path) {
+      this.fotoProfilo = path;
+   }
 
-    /**
-     * @return Password.
-     */
-    public String getPassword() {
-        return password;
-    }
+   /**
+    * @return Password.
+    */
+   public String getPassword() {
+      return password;
+   }
 
-    /**
-     * @param pw Password.
-     */
-    public void setPassword(final String pw) {
-        this.password = pw;
-    }
+   /**
+    * @param pw Password.
+    */
+   public void setPassword(final String pw) {
+      this.password = pw;
+   }
 
-    /**
-     * @return Telefono registrato.
-     */
-    public String getTelefono() {
-        return telefono;
-    }
+   /**
+    * @return Telefono registrato.
+    */
+   public String getTelefono() {
+      return telefono;
+   }
 
-    /**
-     * @param tf Telefono da registrare.
-     */
-    public void setTelefono(final String tf) {
-        this.telefono = tf;
-    }
+   /**
+    * @param tf Telefono da registrare.
+    */
+   public void setTelefono(final String tf) {
+      this.telefono = tf;
+   }
 
-    /**
-     * @return Nome dell'utente.
-     */
-    public String getNome() {
-        return nome;
-    }
+   /**
+    * @return Nome dell'utente.
+    */
+   public String getNome() {
+      return nome;
+   }
 
-    /**
-     * @param name Nome dell'utente.
-     */
-    public void setNome(final String name) {
-        this.nome = name;
-    }
+   /**
+    * @param name Nome dell'utente.
+    */
+   public void setNome(final String name) {
+      this.nome = name;
+   }
 
-    /**
-     * @return Cognome dell'utente.
-     */
-    public String getCognome() {
-        return cognome;
-    }
+   /**
+    * @return Cognome dell'utente.
+    */
+   public String getCognome() {
+      return cognome;
+   }
 
-    /**
-     * @param surname Cognome dell'utente.
-     */
-    public void setCognome(final String surname) {
-        this.cognome = surname;
-    }
+   /**
+    * @param surname Cognome dell'utente.
+    */
+   public void setCognome(final String surname) {
+      this.cognome = surname;
+   }
 
-    /**
-     * @return E-mail dell'utente.
-     */
-    public String getEmail() {
-        return email;
-    }
+   /**
+    * @return E-mail dell'utente.
+    */
+   public String getEmail() {
+      return email;
+   }
 
-    /**
-     * @param em E-mail dell'utente.
-     */
-    public void setEmail(final String em) {
-        this.email = em;
-    }
+   /**
+    * @param em E-mail dell'utente.
+    */
+   public void setEmail(final String em) {
+      this.email = em;
+   }
 
-    /**
-     * @return Strada dell'utente.
-     */
-    public String getStrada() {
-        return strada;
-    }
+   /**
+    * @return Strada dell'utente.
+    */
+   public String getStrada() {
+      return strada;
+   }
 
-    /**
-     * @param st Strada dell'utente.
-     */
-    public void setStrada(final String st) {
-        this.strada = st;
-    }
+   /**
+    * @param st Strada dell'utente.
+    */
+   public void setStrada(final String st) {
+      this.strada = st;
+   }
 
-    /**
-     * @return Città dell'utente.
-     */
-    public String getCitta() {
-        return citta;
-    }
+   /**
+    * @return Città dell'utente.
+    */
+   public String getCitta() {
+      return citta;
+   }
 
-    /**
-     * @param city Città dell'utente.
-     */
-    public void setCitta(final String city) {
-        this.citta = city;
-    }
+   /**
+    * @param city Città dell'utente.
+    */
+   public void setCitta(final String city) {
+      this.citta = city;
+   }
 
-    /**
-     * @return Cap dell'utente.
-     */
-    public String getCap() {
-        return cap;
-    }
+   /**
+    * @return Cap dell'utente.
+    */
+   public String getCap() {
+      return cap;
+   }
 
-    /**
-     * @param cappello Cap dell'utente.
-     */
-    public void setCap(final String cappello) {
-        this.cap = cappello;
-    }
+   /**
+    * @param cappello Cap dell'utente.
+    */
+   public void setCap(final String cappello) {
+      this.cap = cappello;
+   }
 
-    /**
-     * @return Codice Fiscale dell'utente.
-     */
-    public String getCf() {
-        return cf;
-    }
+   /**
+    * @return Codice Fiscale dell'utente.
+    */
+   public String getCf() {
+      return cf;
+   }
 
-    /**
-     * @param codf Codice Fiscale dell'utente.
-     */
-    public void setCf(final String codf) {
-        this.cf = codf;
-    }
+   /**
+    * @param codf Codice Fiscale dell'utente.
+    */
+   public void setCf(final String codf) {
+      this.cf = codf;
+   }
 
-    /**
-     * @return Data Di Nascita dell'utente.
-     */
-    public Date getDataDiNascita() {
-        return dataDiNascita;
-    }
+   /**
+    * @return Data Di Nascita dell'utente.
+    */
+   public Date getDataDiNascita() {
+      return dataDiNascita;
+   }
 
-    /**
-     * @param ddn Data Di Nascita dell'utente.
-     */
-    public void setDataDiNascita(final Date ddn) {
-        this.dataDiNascita = ddn;
-    }
+   /**
+    * @param ddn Data Di Nascita dell'utente.
+    */
+   public void setDataDiNascita(final Date ddn) {
+      this.dataDiNascita = ddn;
+   }
 
-    /**
-     * @return Lista donazioni effettuate dall'utente.
-     */
-    public List<Donazione> getDonazioniList() {
-        return donazioniList;
-    }
+   /**
+    * @return Lista donazioni effettuate dall'utente.
+    */
+   @Override
+   public List<Donazione> getDonazioni() {
+      return donazioni;
+   }
 
-    /**
-     * @param dList Lista donazioni effettuate dall'utente.
-     */
-    public void setDonazioniList(final List<Donazione> dList) {
-        this.donazioniList = dList;
-    }
+   /**
+    * @param dList Lista donazioni effettuate dall'utente.
+    */
+   public void setDonazioni(final List<Donazione> dList) {
+      this.donazioni = dList;
+   }
 
    /**
     * @return Lista effettuate.
@@ -301,17 +302,18 @@ public final class Utente implements UtenteInterface {
       this.segnalazioni = sList;
    }
 
-    /**
-     * @return Lista delle campagne avviate dall'utente.
-     */
-    public List<Campagna> getCampagnaList() {
-        return campagnaList;
-    }
+   /**
+    * @return Lista delle campagne avviate dall'utente.
+    */
+   @Override
+   public List<Campagna> getCampagne() {
+      return campagne;
+   }
 
-    /**
-     * @param cList Lista delle campagne avviate dall'utente.
-     */
-    public void setCampagnaList(final List<Campagna> cList) {
-        this.campagnaList = cList;
-    }
+   /**
+    * @param cList Lista delle campagne avviate dall'utente.
+    */
+   public void setCampagne(final List<Campagna> cList) {
+      this.campagne = cList;
+   }
 }
