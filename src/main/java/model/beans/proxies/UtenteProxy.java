8547e36adf5cf1dc0d221c5a1f1427593d4da9aa
luigi-crescenzo7 <luigi.crescenzo.7@hotmail.it>
2022-01-17 20:26:14 +0100

    Modified UtenteProxy.java and package-info.java

--- src/main/java/model/beans/proxies/UtenteProxy.java
+++ src/main/java/model/beans/proxies/UtenteProxy.java
@@ -4,51 +4,61 @@ package model.beans.proxies;
 import model.DAO.CampagnaDAO;
 import model.DAO.DonazioneDAO;
 import model.DAO.SegnalazioneDAO;
-import model.beans.*;
+
+import model.beans.Campagna;
+import model.beans.Donazione;
+import model.beans.Segnalazione;
+import model.beans.Utente;
 import model.beans.proxyInterfaces.UtenteInterface;
 
 import java.util.List;
 
-public class UtenteProxy implements UtenteInterface {
-   private Utente utente;
+public final class UtenteProxy implements UtenteInterface {
+    /**
+     * Istanza di utente.
+     */
+    private Utente utente;
 
-   public UtenteProxy(Utente u) {
-      this.utente = u;
-   }
+    /**
+     * @param u istanza di Utente
+     */
+    public UtenteProxy(final Utente u) {
+        this.utente = u;
+    }
 
-   @Override
-   public List<Segnalazione> getSegnalazioni() {
-      List<Segnalazione> segnalazioni = utente.getSegnalazioni();
+    @Override
+    public List<Segnalazione> getSegnalazioni() {
+        List<Segnalazione> segnalazioni = utente.getSegnalazioni();
 
-      if (segnalazioni == null) {
-         SegnalazioneDAO dao = new SegnalazioneDAO();
-         utente.setSegnalazioni(dao.getAll());
-      }
+        if (segnalazioni == null) {
+            SegnalazioneDAO dao = new SegnalazioneDAO();
+            utente.setSegnalazioni(dao.getAll());
+        }
 
-      return utente.getSegnalazioni();
-   }
+        return utente.getSegnalazioni();
+    }
 
-   @Override
-   public List<Donazione> getDonazioni() {
-      List<Donazione> donazioni = utente.getDonazioni();
+    @Override
+    public List<Donazione> getDonazioni() {
+        List<Donazione> donazioni = utente.getDonazioni();
 
-      if (donazioni == null) {
-         DonazioneDAO dao = new DonazioneDAO();
-         utente.setDonazioni(dao.getAll());
-      }
+        if (donazioni == null) {
+            DonazioneDAO dao = new DonazioneDAO();
+            utente.setDonazioni(dao.getAll());
+        }
 
-      return utente.getDonazioni();
-   }
+        return utente.getDonazioni();
+    }
 
-   @Override
-   public List<Campagna> getCampagne() {
-      List<Campagna> campagne = utente.getCampagne();
+    @Override
+    public List<Campagna> getCampagne() {
+        List<Campagna> campagne = utente.getCampagne();
 
-      if (campagne == null) {
-         CampagnaDAO dao = new CampagnaDAO();
-         utente.setCampagne(dao.getAll());
-      }
+        if (campagne == null) {
+            CampagnaDAO dao = new CampagnaDAO();
+            utente.setCampagne(dao.getAll());
+        }
 
-      return utente.getCampagne();
-   }
+        return utente.getCampagne();
+    }
 }
