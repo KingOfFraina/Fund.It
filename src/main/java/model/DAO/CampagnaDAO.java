15ad5cced26b0b93bd33b38e2d064d65909d4c27
luigi-crescenzo7 <luigi.crescenzo.7@hotmail.it>
2022-01-18 15:16:28 +0100

    modified CampagnaDAO.java and UtenteDAO.java

--- src/main/java/model/DAO/CampagnaDAO.java
+++ src/main/java/model/DAO/CampagnaDAO.java
@@ -16,166 +16,166 @@ import java.util.List;
 
 public final class CampagnaDAO
         implements DAO<Campagna> {
-   /**
-    * @param id rappresenta l'identificativo dell'entity
-    * @return null se non viene trovato nessun risultato,
-    * un'istanza di T nel caso in cui viene trovato un risultato
-    */
-   @Override
-   public Campagna getById(final int id) {
-      try (Connection connection = ConPool.getInstance().getConnection();
-           PreparedStatement statement = connection.prepareStatement("SELECT"
-                   + " * FROM campagna AS c"
-                   + " WHERE idCampagna = ?")) {
-         int index = 1;
+    /**
+     * @param id rappresenta l'identificativo dell'entity
+     * @return null se non viene trovato nessun risultato,
+     * un'istanza di T nel caso in cui viene trovato un risultato
+     */
+    @Override
+    public Campagna getById(final int id) {
+        try (Connection connection = ConPool.getInstance().getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT"
+                     + " * FROM campagna AS c"
+                     + " WHERE idCampagna = ?")) {
+            int index = 1;
 
-         statement.setInt(index, id);
+            statement.setInt(index, id);
 
-         ResultSet set = statement.executeQuery();
-         if (set.next()) {
-            return extract(set, "c");
-         }
+            ResultSet set = statement.executeQuery();
+            if (set.next()) {
+                return extract(set, "c");
+            }
 
-      } catch (SQLException e) {
-         throw new RuntimeException(e);
-      }
-      return null;
-   }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 
-   /**
-    * @return una lista di entity T
-    */
-   @Override
-   public List<Campagna> getAll() {
-      List<Campagna> list = new ArrayList<>();
-      try (Connection connection = ConPool.getInstance().getConnection();
-           PreparedStatement statement =
-                   connection.prepareStatement("SELECT * "
-                           + "FROM campagna AS c ")) {
-         ResultSet set = statement.executeQuery();
-         while (set.next()) {
-            list.add(extract(set, "c"));
-         }
-      } catch (SQLException e) {
-         throw new RuntimeException(e);
-      }
-      return list;
-   }
+    /**
+     * @return una lista di entity T
+     */
+    @Override
+    public List<Campagna> getAll() {
+        List<Campagna> list = new ArrayList<>();
+        try (Connection connection = ConPool.getInstance().getConnection();
+             PreparedStatement statement =
+                     connection.prepareStatement("SELECT * "
+                             + "FROM campagna AS c ")) {
+            ResultSet set = statement.executeQuery();
+            while (set.next()) {
+                list.add(extract(set, "c"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
 
-   /**
-    * @param entity l'istanza da salvare
-    * @return false --> se l'operazione non va a buon fine,
-    * true --> se l'operazione va a buon fine
-    */
-   @Override
-   public boolean save(final Campagna entity) {
-      int ret;
-      try (Connection connection = ConPool.getInstance().getConnection();
-           PreparedStatement statement =
-                   connection.prepareStatement("INSERT INTO campagna "
-                                   + "(STATO, TITOLO, DESCRIZIONE,"
-                                   + " SOMMARACCOLTA, SOMMATARGET,"
-                                   + " IDCATEGORIA, IDUTENTE)"
-                                   + "VALUES (?,?,?,?,?,?,?)",
-                           PreparedStatement.RETURN_GENERATED_KEYS)) {
+    /**
+     * @param entity l'istanza da salvare
+     * @return false --> se l'operazione non va a buon fine,
+     * true --> se l'operazione va a buon fine
+     */
+    @Override
+    public boolean save(final Campagna entity) {
+        int ret;
+        try (Connection connection = ConPool.getInstance().getConnection();
+             PreparedStatement statement =
+                     connection.prepareStatement("INSERT INTO campagna "
+                                     + "(STATO, TITOLO, DESCRIZIONE,"
+                                     + " SOMMARACCOLTA, SOMMATARGET,"
+                                     + " IDCATEGORIA, IDUTENTE)"
+                                     + "VALUES (?,?,?,?,?,?,?)",
+                             PreparedStatement.RETURN_GENERATED_KEYS)) {
 
-         int index = 1;
-         statement.setInt(index++, entity.getIdCampagna());
-         statement.setString(index++, entity.getStato().toString());
-         statement.setString(index++, entity.getTitolo());
-         statement.setString(index++, entity.getDescrizione());
-         statement.setDouble(index++, entity.getSommaRaccolta());
-         statement.setDouble(index++, entity.getSommaTarget());
-         statement.setInt(index++,
-                 entity.getCategoria().getIdCategoria());
-         statement.setInt(index++, entity.getUtente().getIdUtente());
+            int index = 1;
+            statement.setInt(index++, entity.getIdCampagna());
+            statement.setString(index++, entity.getStato().toString());
+            statement.setString(index++, entity.getTitolo());
+            statement.setString(index++, entity.getDescrizione());
+            statement.setDouble(index++, entity.getSommaRaccolta());
+            statement.setDouble(index++, entity.getSommaTarget());
+            statement.setInt(index++,
+                    entity.getCategoria().getIdCategoria());
+            statement.setInt(index++, entity.getUtente().getIdUtente());
 
-         ret = statement.executeUpdate();
-         ResultSet set = statement.getGeneratedKeys();
-         if (set.next()) {
-            entity.setIdCampagna(set.getInt(0));
-         }
-      } catch (SQLException e) {
-         throw new RuntimeException(e);
-      }
-      return ret > 0;
-   }
+            ret = statement.executeUpdate();
+            ResultSet set = statement.getGeneratedKeys();
+            if (set.next()) {
+                entity.setIdCampagna(set.getInt(0));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return ret > 0;
+    }
 
-   /**
-    * @param entity l'istanza da aggiornare
-    * @return false --> se l'operazione non va a buon fine,
-    * true --> se l'operazione va a buon fine
-    */
-   @Override
-   public boolean update(final Campagna entity) {
-      int ret = 0;
-      try (Connection connection = ConPool.getInstance().getConnection();
-           PreparedStatement statement =
-                   connection.prepareStatement("UPDATE campagna SET"
-                           + " Stato = ?, titolo = ?, descrizione = ?,"
-                           + " sommaRaccolta = ?, sommaTarget = ?,"
-                           + " idCategoria = ?, idUtente = ?"
-                           + " WHERE idCampagna = ?")) {
-         int index = 1;
-         statement.setString(index++, entity.getStato().toString());
-         statement.setString(index++, entity.getTitolo());
-         statement.setString(index++, entity.getDescrizione());
-         statement.setDouble(index++, entity.getSommaRaccolta());
-         statement.setDouble(index++, entity.getSommaTarget());
-         statement.setInt(index++, entity.getCategoria().getIdCategoria());
+    /**
+     * @param entity l'istanza da aggiornare
+     * @return false --> se l'operazione non va a buon fine,
+     * true --> se l'operazione va a buon fine
+     */
+    @Override
+    public boolean update(final Campagna entity) {
+        int ret = 0;
+        try (Connection connection = ConPool.getInstance().getConnection();
+             PreparedStatement statement =
+                     connection.prepareStatement("UPDATE campagna SET"
+                             + " Stato = ?, titolo = ?, descrizione = ?,"
+                             + " sommaRaccolta = ?, sommaTarget = ?,"
+                             + " idCategoria = ?, idUtente = ?"
+                             + " WHERE idCampagna = ?")) {
+            int index = 1;
+            statement.setString(index++, entity.getStato().toString());
+            statement.setString(index++, entity.getTitolo());
+            statement.setString(index++, entity.getDescrizione());
+            statement.setDouble(index++, entity.getSommaRaccolta());
+            statement.setDouble(index++, entity.getSommaTarget());
+            statement.setInt(index++, entity.getCategoria().getIdCategoria());
 
-         ret = statement.executeUpdate();
-      } catch (SQLException e) {
-         throw new RuntimeException(e);
-      }
-      return ret > 0;
-   }
+            ret = statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return ret > 0;
+    }
 
-   /**
-    * @param entity l'istanza da eliminare
-    * @return false --> se l'operazione non va a buon fine,
-    * true --> se l'operazione va a buon fine
-    */
-   @Override
-   public boolean delete(final Campagna entity) {
-      int ret = 0;
-      try (Connection connection = ConPool.getInstance().getConnection();
-           PreparedStatement statement =
-                   connection.prepareStatement("DELETE FROM campagna"
-                           + " WHERE idCampagna = ?")) {
-         statement.setInt(1, entity.getIdCampagna());
-         ret = statement.executeUpdate();
+    /**
+     * @param entity l'istanza da eliminare
+     * @return false --> se l'operazione non va a buon fine,
+     * true --> se l'operazione va a buon fine
+     */
+    @Override
+    public boolean delete(final Campagna entity) {
+        int ret = 0;
+        try (Connection connection = ConPool.getInstance().getConnection();
+             PreparedStatement statement =
+                     connection.prepareStatement("DELETE FROM campagna"
+                             + " WHERE idCampagna = ?")) {
+            statement.setInt(1, entity.getIdCampagna());
+            ret = statement.executeUpdate();
 
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
-      return ret > 0;
-   }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ret > 0;
+    }
 
-   /**
-    * @param resultSet resultSet della query eseguita
-    * @param alias     eventuale alias del field
-    * @return l'istanza della della classe T popolata con le informazioni
-    * presenti nel resultSet
-    * @throws SQLException eccezione lanciata in caso di problemi
-    */
-   @Override
-   public Campagna extract(final ResultSet resultSet, final String alias)
-           throws SQLException {
-      Campagna c = new Campagna();
-      c.setIdCampagna(resultSet.getInt(alias + ".idCampagna"));
-      c.setStato(StatoCampagna.valueOf(
-              resultSet.getString(alias + ".Stato").toUpperCase()));
-      c.setTitolo(resultSet.getString(alias + ".titolo"));
-      c.setDescrizione(resultSet.getString(alias + ".descrizione"));
-      c.setSommaRaccolta(resultSet.getDouble(alias + ".sommaRaccolta"));
-      c.setSommaTarget(resultSet.getDouble(alias + ".sommaTarget"));
-      Categoria c1 = new Categoria();
-      c1.setIdCategoria(resultSet.getInt(alias + ".idCategoria"));
-      c.setCategoria(c1);
-      Utente utente = new Utente();
-      utente.setIdUtente(resultSet.getInt(alias + ".idUtente"));
-      c.setUtente(utente);
-      return c;
-   }
+    /**
+     * @param resultSet resultSet della query eseguita
+     * @param alias     eventuale alias del field
+     * @return l'istanza della della classe T popolata con le informazioni
+     * presenti nel resultSet
+     * @throws SQLException eccezione lanciata in caso di problemi
+     */
+    @Override
+    public Campagna extract(final ResultSet resultSet, final String alias)
+            throws SQLException {
+        Campagna c = new Campagna();
+        c.setIdCampagna(resultSet.getInt(alias + ".idCampagna"));
+        c.setStato(StatoCampagna.valueOf(
+                resultSet.getString(alias + ".Stato").toUpperCase()));
+        c.setTitolo(resultSet.getString(alias + ".titolo"));
+        c.setDescrizione(resultSet.getString(alias + ".descrizione"));
+        c.setSommaRaccolta(resultSet.getDouble(alias + ".sommaRaccolta"));
+        c.setSommaTarget(resultSet.getDouble(alias + ".sommaTarget"));
+        Categoria c1 = new Categoria();
+        c1.setIdCategoria(resultSet.getInt(alias + ".idCategoria"));
+        c.setCategoria(c1);
+        Utente utente = new Utente();
+        utente.setIdUtente(resultSet.getInt(alias + ".idUtente"));
+        c.setUtente(utente);
+        return c;
+    }
 }
--- src/main/java/model/DAO/UtenteDAO.java
+++ src/main/java/model/DAO/UtenteDAO.java
@@ -12,194 +12,194 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class UtenteDAO implements DAO<Utente> {
-   @Override
-   public Utente getById(final int id) {
-      Utente utente = null;
+    @Override
+    public Utente getById(final int id) {
+        Utente utente = null;
 
-      try (Connection connection = ConPool.getInstance().getConnection()) {
-         if (connection != null) {
-            String query = "SELECT * FROM utente WHERE idUtente = ?";
-
-            try (PreparedStatement preparedStatement =
-                         connection.prepareStatement(query)) {
-               preparedStatement.setInt(1, id);
-               ResultSet resultSet = preparedStatement.executeQuery();
-
-               if (resultSet.next()) {
-                  utente = extract(resultSet, null);
-               }
-            }
-         }
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
-
-      return utente;
-   }
-
-   @Override
-   public List<Utente> getAll() {
-      List<Utente> utenteList = null;
-
-      try (Connection connection = ConPool.getInstance().getConnection()) {
-         if (connection != null) {
-            String query = "SELECT * FROM utente";
-            utenteList = new ArrayList<>();
-
-            try (PreparedStatement preparedStatement =
-                         connection.prepareStatement(query)) {
-               ResultSet resultSet = preparedStatement.executeQuery();
-
-               while (resultSet.next()) {
-                  utenteList.add(extract(resultSet, null));
-               }
-
-            }
-         }
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
-
-      return utenteList;
-   }
-
-   @Override
-   public boolean update(final Utente entity) {
-      if (entity != null) {
-         try (Connection connection =
-                      ConPool.getInstance().getConnection()) {
+        try (Connection connection = ConPool.getInstance().getConnection()) {
             if (connection != null) {
-               String query =
-                       "UPDATE utente SET dataBan = ?,admin = ?,fotoProfilo = ?"
-                               + ", password = ?, telefono = ?, nome = ?, "
-                               + "cognome = ?, email = ?, strada = ?, città = ?"
-                               + ", cap = ?, cf = ?, dataDiNascita = ?"
-                               + "WHERE idUtente = ?";
+                String query = "SELECT * FROM utente WHERE idUtente = ?";
 
-               try (PreparedStatement preparedStatement =
-                            connection.prepareStatement(query)) {
+                try (PreparedStatement preparedStatement =
+                             connection.prepareStatement(query)) {
+                    preparedStatement.setInt(1, id);
+                    ResultSet resultSet = preparedStatement.executeQuery();
 
-                  int index = fillPreparedStatement(preparedStatement, entity);
-                  preparedStatement.setInt(index, entity.getIdUtente());
-
-                  return preparedStatement.executeUpdate() > 0;
-               }
+                    if (resultSet.next()) {
+                        utente = extract(resultSet, null);
+                    }
+                }
             }
-         } catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-         }
-      }
-      return false;
-   }
+        }
 
-   @Override
-   public boolean save(final Utente entity) {
-      if (entity != null) {
-         try (Connection connection =
-                      ConPool.getInstance().getConnection()) {
+        return utente;
+    }
+
+    @Override
+    public List<Utente> getAll() {
+        List<Utente> utenteList = null;
+
+        try (Connection connection = ConPool.getInstance().getConnection()) {
             if (connection != null) {
-               String query =
-                       "INSERT INTO utente (dataBan, admin, fotoProfilo, "
-                               + "password, telefono, nome, cognome, email,"
-                               + " strada, città, cap, cf, dataDiNascita) "
-                               + "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                String query = "SELECT * FROM utente";
+                utenteList = new ArrayList<>();
 
-               try (PreparedStatement preparedStatement =
-                            connection.prepareStatement(query)) {
+                try (PreparedStatement preparedStatement =
+                             connection.prepareStatement(query)) {
+                    ResultSet resultSet = preparedStatement.executeQuery();
 
-                  fillPreparedStatement(preparedStatement, entity);
+                    while (resultSet.next()) {
+                        utenteList.add(extract(resultSet, null));
+                    }
 
-                  return preparedStatement.executeUpdate() > 0;
-               }
+                }
             }
-         } catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-         }
-      }
-      return false;
-   }
+        }
 
-   @Override
-   public boolean delete(final Utente entity) {
-      if (entity != null) {
-         try (Connection connection =
-                      ConPool.getInstance().getConnection()) {
-            if (connection != null) {
-               String query =
-                       "DELETE FROM utente WHERE idUtente = ?";
+        return utenteList;
+    }
 
-               try (PreparedStatement preparedStatement =
-                            connection.prepareStatement(query)) {
+    @Override
+    public boolean update(final Utente entity) {
+        if (entity != null) {
+            try (Connection connection =
+                         ConPool.getInstance().getConnection()) {
+                if (connection != null) {
+                    String query =
+                            "UPDATE utente SET dataBan = ?,admin = ?,fotoProfilo = ?"
+                                    + ", password = ?, telefono = ?, nome = ?, "
+                                    + "cognome = ?, email = ?, strada = ?, città = ?"
+                                    + ", cap = ?, cf = ?, dataDiNascita = ?"
+                                    + "WHERE idUtente = ?";
 
-                  preparedStatement.setInt(1, entity.getIdUtente());
+                    try (PreparedStatement preparedStatement =
+                                 connection.prepareStatement(query)) {
 
-                  return preparedStatement.executeUpdate() > 0 ? true : false;
-               }
+                        int index = fillPreparedStatement(preparedStatement, entity);
+                        preparedStatement.setInt(index, entity.getIdUtente());
+
+                        return preparedStatement.executeUpdate() > 0;
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-         } catch (SQLException e) {
-            e.printStackTrace();
-         }
-      }
-      return false;
-   }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean save(final Utente entity) {
+        if (entity != null) {
+            try (Connection connection =
+                         ConPool.getInstance().getConnection()) {
+                if (connection != null) {
+                    String query =
+                            "INSERT INTO utente (dataBan, admin, fotoProfilo, "
+                                    + "password, telefono, nome, cognome, email,"
+                                    + " strada, città, cap, cf, dataDiNascita) "
+                                    + "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+                    try (PreparedStatement preparedStatement =
+                                 connection.prepareStatement(query)) {
+
+                        fillPreparedStatement(preparedStatement, entity);
+
+                        return preparedStatement.executeUpdate() > 0;
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(final Utente entity) {
+        if (entity != null) {
+            try (Connection connection =
+                         ConPool.getInstance().getConnection()) {
+                if (connection != null) {
+                    String query =
+                            "DELETE FROM utente WHERE idUtente = ?";
+
+                    try (PreparedStatement preparedStatement =
+                                 connection.prepareStatement(query)) {
+
+                        preparedStatement.setInt(1, entity.getIdUtente());
+
+                        return preparedStatement.executeUpdate() > 0;
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
 
 
-   @Override
-   public Utente extract(final ResultSet resultSet, final String alias)
-           throws SQLException {
-      Utente utente = null;
-      String tableAlias = "";
+    @Override
+    public Utente extract(final ResultSet resultSet, final String alias)
+            throws SQLException {
+        Utente utente = null;
+        String tableAlias = "";
 
-      if (resultSet != null) {
-         utente = new Utente();
+        if (resultSet != null) {
+            utente = new Utente();
 
-         if (alias != null) {
-            tableAlias = alias + ".";
-         }
+            if (alias != null) {
+                tableAlias = alias + ".";
+            }
 
-         utente.setAdmin(resultSet.getBoolean(tableAlias + "admin"));
-         utente.setCap(resultSet.getString(tableAlias + "cap"));
-         utente.setCf(resultSet.getString(tableAlias + "cf"));
-         utente.setCitta(resultSet.getString(tableAlias + "città"));
-         utente.setCognome(resultSet.getString(tableAlias + "cognome"));
-         utente.setDataBan(resultSet.getDate(tableAlias + "dataBan"));
-         utente.setDataDiNascita(resultSet.getDate(tableAlias
-                 + "dataDiNascita"));
-         utente.setEmail(resultSet.getString(tableAlias + "email"));
-         utente.setFotoProfilo(resultSet.getString(tableAlias + "fotoProfilo"));
-         utente.setIdUtente(resultSet.getInt(tableAlias + "idUtente"));
-         utente.setNome(resultSet.getString(tableAlias + "nome"));
-         utente.setPassword(resultSet.getString(tableAlias + "password"));
-         utente.setStrada(resultSet.getString(tableAlias + "strada"));
-         utente.setTelefono(resultSet.getString(tableAlias + "telefono"));
-         utente.setCampagne(null);
-         utente.setDonazioni(null);
-         utente.setSegnalazioni(null);
-      }
+            utente.setAdmin(resultSet.getBoolean(tableAlias + "admin"));
+            utente.setCap(resultSet.getString(tableAlias + "cap"));
+            utente.setCf(resultSet.getString(tableAlias + "cf"));
+            utente.setCitta(resultSet.getString(tableAlias + "città"));
+            utente.setCognome(resultSet.getString(tableAlias + "cognome"));
+            utente.setDataBan(resultSet.getDate(tableAlias + "dataBan"));
+            utente.setDataDiNascita(resultSet.getDate(tableAlias
+                    + "dataDiNascita"));
+            utente.setEmail(resultSet.getString(tableAlias + "email"));
+            utente.setFotoProfilo(resultSet.getString(tableAlias + "fotoProfilo"));
+            utente.setIdUtente(resultSet.getInt(tableAlias + "idUtente"));
+            utente.setNome(resultSet.getString(tableAlias + "nome"));
+            utente.setPassword(resultSet.getString(tableAlias + "password"));
+            utente.setStrada(resultSet.getString(tableAlias + "strada"));
+            utente.setTelefono(resultSet.getString(tableAlias + "telefono"));
+            utente.setCampagne(null);
+            utente.setDonazioni(null);
+            utente.setSegnalazioni(null);
+        }
 
-      return utente;
-   }
+        return utente;
+    }
 
-   private int fillPreparedStatement(final PreparedStatement preparedStatement,
-                                     final Utente entity) throws SQLException {
-      int index = 1;
+    private int fillPreparedStatement(final PreparedStatement preparedStatement,
+                                      final Utente entity) throws SQLException {
+        int index = 1;
 
-      preparedStatement.setDate(index++,
-              (Date) entity.getDataBan());
-      preparedStatement.setBoolean(index++, entity.isAdmin());
-      preparedStatement.setString(index++, entity.getFotoProfilo());
-      preparedStatement.setString(index++, entity.getPassword());
-      preparedStatement.setString(index++, entity.getTelefono());
-      preparedStatement.setString(index++, entity.getNome());
-      preparedStatement.setString(index++, entity.getCognome());
-      preparedStatement.setString(index++, entity.getEmail());
-      preparedStatement.setString(index++, entity.getStrada());
-      preparedStatement.setString(index++, entity.getCitta());
-      preparedStatement.setString(index++, entity.getCap());
-      preparedStatement.setString(index++, entity.getCf());
-      preparedStatement.setDate(index++,
-              (Date) entity.getDataDiNascita());
+        preparedStatement.setDate(index++,
+                (Date) entity.getDataBan());
+        preparedStatement.setBoolean(index++, entity.isAdmin());
+        preparedStatement.setString(index++, entity.getFotoProfilo());
+        preparedStatement.setString(index++, entity.getPassword());
+        preparedStatement.setString(index++, entity.getTelefono());
+        preparedStatement.setString(index++, entity.getNome());
+        preparedStatement.setString(index++, entity.getCognome());
+        preparedStatement.setString(index++, entity.getEmail());
+        preparedStatement.setString(index++, entity.getStrada());
+        preparedStatement.setString(index++, entity.getCitta());
+        preparedStatement.setString(index++, entity.getCap());
+        preparedStatement.setString(index++, entity.getCf());
+        preparedStatement.setDate(index++,
+                (Date) entity.getDataDiNascita());
 
-      return index;
-   }
+        return index;
+    }
 }
