ae4cecc7ae39c48832d77ebc0557e8648159423f
Francesco <francesco.expo00@gmail.com>
2022-01-19 20:03:43 +0100

    DonazioniService, DonazioniServiceImp checkstyle

--- src/main/java/model/services/DonazioniServiceImp.java
+++ src/main/java/model/services/DonazioniServiceImp.java
@@ -6,26 +6,31 @@ import model.beans.Utente;
 
 import java.util.List;
 
-public class DonazioniServiceImp implements DonazioniService{
-
+public final class DonazioniServiceImp implements DonazioniService {
+    /**
+     * dao utilizzato.
+     */
     private DonazioneDAO dao;
 
+    /**
+     * costruttore.
+     */
     public DonazioniServiceImp() {
         this.dao = new DonazioneDAO();
     }
 
     @Override
-    public boolean effettuaDonazione(Donazione d) {
+    public boolean effettuaDonazione(final Donazione d) {
         return dao.save(d);
     }
 
     @Override
-    public List<Donazione> visualizzaDonazioni(Utente u) {
+    public List<Donazione> visualizzaDonazioni(final Utente u) {
         return dao.getAllByUtente(u.getIdUtente());
     }
 
     @Override
-    public boolean commenta(Donazione d) {
+    public boolean commenta(final Donazione d) {
         return dao.update(d);
     }
 }
