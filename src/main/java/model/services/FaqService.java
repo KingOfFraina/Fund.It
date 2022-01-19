3cb06d77564338cd2cc186d0fa7b5b3030b88ebb
Sabato Genovese <48244524+SabatoGenovese@users.noreply.github.com>
2022-01-19 18:49:51 +0100

    Scritta interfaccia CampagnaService.java

--- src/main/java/model/services/FaqService.java
+++ src/main/java/model/services/FaqService.java
@@ -3,7 +3,27 @@ package model.services;
 import model.beans.FAQ;
 
 public interface FaqService {
+   /**
+    * Inserisce una nuova FAQ.
+    *
+    * @param faq bean che contiene informazioni sulle faq
+    * @return l'esito con cui si è conclusa l'operazione
+    */
    boolean inserisciFaq(FAQ faq);
+
+   /**
+    * Modifica la FAQ esistente.
+    *
+    * @param faq bean che contiene informazioni sulle faq
+    * @return l'esito con cui si è conclusa l'operazione
+    */
    boolean cancellaFaq(FAQ faq);
+
+   /**
+    * Elimina una FAQ esistente.
+    *
+    * @param faq bean che contiene informazioni sulle faq
+    * @return l'esito con cui si è conclusa l'operazione
+    */
    boolean modificaFaq(FAQ faq);
 }
