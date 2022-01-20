f112b26fc0cb2642a5ab65e0c5be7e8d5cc8fbb3
Sabato Genovese <48244524+SabatoGenovese@users.noreply.github.com>
2022-01-20 18:01:33 +0100

    Fix UtenteService.java

--- src/main/java/model/services/UtenteService.java
+++ src/main/java/model/services/UtenteService.java
@@ -5,35 +5,35 @@ import model.beans.Utente;
 import java.util.List;
 
 public interface UtenteService {
-    /**
-     * @param id
-     * @return TODO
-     */
-    Utente visualizzaDashboardUtente(int id);
+   /**
+    * @param id idUtente.
+    * @return l'istanza di utente presente nel database
+    */
+   Utente visualizzaDashboardUtente(int id);
 
-    /**
-     * @param utente da modificare.
-     * @return esito operazione.
-     */
-    boolean modificaProfilo(Utente utente);
+   /**
+    * @param utente da modificare.
+    * @return esito operazione.
+    */
+   boolean modificaProfilo(Utente utente);
 
-    /**
-     * @param richiedente della lista completa utenti.
-     * @return null se il richiedente non è admin. List di Utente se è admin.
-     */
-    List<Utente> visualizzaUtenti(int richiedente);
+   /**
+    * @param richiedente della lista completa utenti.
+    * @return null se il richiedente non è admin. List di Utente se è admin.
+    */
+   List<Utente> visualizzaUtenti(int richiedente);
 
-    /**
-     * @param richiedente idUtente di chi ha ordinato l'operazione.
-     * @param soggetto    idUtente di cui va modificato lo stato.
-     * @return esito dell'operazione.
-     */
-    boolean promuoviDeclassaUtente(int richiedente, int soggetto);
+   /**
+    * @param richiedente idUtente di chi ha ordinato l'operazione.
+    * @param soggetto    idUtente di cui va modificato lo stato.
+    * @return esito dell'operazione.
+    */
+   boolean promuoviDeclassaUtente(int richiedente, int soggetto);
 
-    /**
-     * @param idCattivone da bannare.
-     * @return esito operazione.
-     */
-    boolean sospensioneUtente(int idCattivone);
+   /**
+    * @param idCattivone da bannare.
+    * @return esito operazione.
+    */
+   boolean sospensioneUtente(int idCattivone);
 
 }
--- src/main/java/model/services/UtenteServiceImp.java
+++ src/main/java/model/services/UtenteServiceImp.java
@@ -7,79 +7,79 @@ import java.util.Date;
 import java.util.List;
 
 public class UtenteServiceImp implements UtenteService {
-    /**
-     * @param id idUtente.
-     * @return TODO
-     */
-    @Override
-    public Utente visualizzaDashboardUtente(final int id) {
-        return null;
-    }
+   /**
+    * @param id idUtente.
+    * @return l'istanza di utente presente nel database
+    */
+   @Override
+   public Utente visualizzaDashboardUtente(final int id) {
+      return new UtenteDAO().getById(id);
+   }
 
-    /**
-     * @param utente da modificare.
-     * @return esito operazione.
-     */
-    @Override
-    public boolean modificaProfilo(final Utente utente) {
-        UtenteDAO utenteDAO = new UtenteDAO();
-        return utenteDAO.update(utente);
-    }
+   /**
+    * @param utente da modificare.
+    * @return esito operazione.
+    */
+   @Override
+   public boolean modificaProfilo(final Utente utente) {
+      UtenteDAO utenteDAO = new UtenteDAO();
+      return utenteDAO.update(utente);
+   }
 
-    /**
-     * @param richiedente della lista completa utenti.
-     * @return null se il richiedente non è admin. List di Utente se è admin.
-     */
-    @Override
-    public List<Utente> visualizzaUtenti(final int richiedente) {
-        if (richiedente < 0) {
-            return null;
-        }
-        UtenteDAO dao = new UtenteDAO();
-        if (!dao.getById(richiedente).isAdmin()) {
-            return null;
-        }
+   /**
+    * @param richiedente della lista completa utenti.
+    * @return null se il richiedente non è admin. List di Utente se è admin.
+    */
+   @Override
+   public List<Utente> visualizzaUtenti(final int richiedente) {
+      if (richiedente < 0) {
+         return null;
+      }
+      UtenteDAO dao = new UtenteDAO();
+      if (!dao.getById(richiedente).isAdmin()) {
+         return null;
+      }
 
-        List<Utente> list = dao.getAll();
+      List<Utente> list = dao.getAll();
 
-        return list;
-    }
+      return list;
+   }
 
-    /**
-     * @param richiedente idUtente di chi ha ordinato l'operazione.
-     * @param soggetto    idUtente di cui va modificato lo stato.
-     * @return esito dell'operazione.
-     */
-    @Override
-    public boolean promuoviDeclassaUtente(final int richiedente,
-                                          final int soggetto) {
-        if (richiedente < 0) {
-            return false;
-        }
-        UtenteDAO dao = new UtenteDAO();
-        if (!dao.getById(richiedente).isAdmin()) {
-            return false;
-        }
+   /**
+    * @param richiedente idUtente di chi ha ordinato l'operazione.
+    * @param soggetto    idUtente di cui va modificato lo stato.
+    * @return esito dell'operazione.
+    */
+   @Override
+   public boolean promuoviDeclassaUtente(final int richiedente,
+                                         final int soggetto) {
+      if (richiedente < 0) {
+         return false;
+      }
+      UtenteDAO dao = new UtenteDAO();
+      if (!dao.getById(richiedente).isAdmin()) {
+         return false;
+      }
 
-        Utente sogg = dao.getById(soggetto);
-        if (sogg.isAdmin()) {
-            sogg.setAdmin(false);
-        } else {
-            sogg.setAdmin(true);
-        }
-        dao.update(sogg);
-        return false;
-    }
+      Utente sogg = dao.getById(soggetto);
+      if (sogg.isAdmin()) {
+         sogg.setAdmin(false);
+      } else {
+         sogg.setAdmin(true);
+      }
+      dao.update(sogg);
+      return false;
+   }
 
-    /**
-     * @param idCattivone da bannare.
-     * @return esito operazione.
-     */
-    @Override
-    public boolean sospensioneUtente(final int idCattivone) {
-        UtenteDAO utenteDAO = new UtenteDAO();
-        Utente utente = utenteDAO.getById(idCattivone);
-        utente.setDataBan(new Date());
-        return utenteDAO.update(utente);
-    }
+   /**
+    * @param idCattivone da bannare.
+    * @return esito operazione.
+    */
+   @Override
+   public boolean sospensioneUtente(final int idCattivone) {
+      UtenteDAO utenteDAO = new UtenteDAO();
+      Utente utente = utenteDAO.getById(idCattivone);
+      utente.setDataBan(new Date());
+      return utenteDAO.update(utente);
+   }
 }
