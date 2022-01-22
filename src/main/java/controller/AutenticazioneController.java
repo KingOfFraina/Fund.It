6cc0dffebe7298a0918161ef0a23a7e6c4c9a1ed
luigi-crescenzo7 <luigi.crescenzo.7@hotmail.it>
2022-01-22 17:16:23 +0100

    checkstyle fix to AutenticazioneController.java

--- src/main/java/controller/AutenticazioneController.java
+++ src/main/java/controller/AutenticazioneController.java
@@ -1,81 +1,84 @@
 package controller;
 
-import model.beans.Segnalazione;
 import model.beans.Utente;
 import model.services.AutenticazioneServiceImpl;
-import model.services.SegnalazioniServiceImpl;
-import model.storage.ConPool;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "AutenticazioneController", value = "/AutenticazioneController/*")
-public class AutenticazioneController extends HttpServlet {
+@WebServlet(name = "AutenticazioneController",
+        value = "/AutenticazioneController/*")
+public final class AutenticazioneController extends HttpServlet {
 
-   @Override
-   protected void doGet(HttpServletRequest request, HttpServletResponse response)
-           throws ServletException, IOException {
+    @Override
+    protected void doGet(final HttpServletRequest request,
+                         final HttpServletResponse response)
+            throws ServletException, IOException {
 
-      String path = request.getPathInfo();
+        String path = request.getPathInfo();
 
-      String resource = "/";
+        String resource = "/";
 
 
-      if (path.equals("/login")) {
-         resource = "/login.jsp";
-      } else if (path.equals("/registrazione")) {
-         resource = "/registrazione.jsp";
-      } else {
-         response.sendError(HttpServletResponse.SC_NOT_FOUND, "Risorsa non trovata");
-         return
-      }
-
-      switch (path) {
-         case "/login":
+        if (path.equals("/login")) {
             resource = "/login.jsp";
-            break;
-         case "/registrazione":
+        } else if (path.equals("/registrazione")) {
             resource = "/registrazione.jsp";
-            break;
-         default:
+        } else {
+            response.sendError(
+                    HttpServletResponse.SC_NOT_FOUND, "Risorsa non trovata");
+            return;
+        }
 
-      }
+        RequestDispatcher dispatcher =
+                request.getRequestDispatcher("/WEB-INF/results" + resource);
+        dispatcher.forward(request, response);
 
-      RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/results" + resource);
-      dispatcher.forward(request, response);
+    }
 
-   }
+    @Override
+    protected void doPost(final HttpServletRequest request,
+                          final HttpServletResponse response)
+            throws ServletException, IOException {
 
-   @Override
-   protected void doPost(HttpServletRequest request, HttpServletResponse response)
-           throws ServletException, IOException {
+        switch (request.getPathInfo()) {
+            case "/login":
+                login(request, response);
+                break;
+            default:
+                System.out.println("default clause");
+                break;
+        }
+    }
 
-      switch (request.getPathInfo()) {
-         case "/login":
-            login(request, response);
-            break;
-      }
-   }
+    private void login(final HttpServletRequest request,
+                       final HttpServletResponse response)
+            throws IOException {
+        HttpSession session = request.getSession(true);
+        Utente utente = (Utente) session.getAttribute("utente");
 
-   private void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      HttpSession session = request.getSession(true);
-      Utente utente = (Utente) session.getAttribute("utente");
+        if (utente == null) {
+            utente = new Utente();
+            utente.setEmail(request.getParameter("email"));
+            utente.createPasswordHash(request.getParameter("password"));
 
-      if (utente == null) {
-         utente = new Utente();
-         utente.setEmail(request.getParameter("email"));
-         utente.createPasswordHash(request.getParameter("password"));
+            AutenticazioneServiceImpl autenticazioneService =
+                    new AutenticazioneServiceImpl(request.getSession(true));
+            utente = autenticazioneService.login(utente);
+        }
 
-         AutenticazioneServiceImpl autenticazioneService = new AutenticazioneServiceImpl(request.getSession(true));
-         utente = autenticazioneService.login(utente);
-      }
-
-      if (utente != null) {
-         session.setAttribute("utente", utente);
-      } else {
-         response.sendRedirect(getServletContext().getContextPath() + "/AutenticazioneController/login");
-      }
-   }
+        if (utente != null) {
+            session.setAttribute("utente", utente);
+        } else {
+            response.sendRedirect(
+                    getServletContext().getContextPath()
+                            + "/AutenticazioneController/login");
+        }
+    }
 }
