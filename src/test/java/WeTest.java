6b06f941ef02ca13d940c8d2b1ff185c23c1024e
luigi-crescenzo7 <luigi.crescenzo.7@hotmail.it>
2022-01-30 15:25:03 +0100

    WeTest.java added

---
+++ src/test/java/WeTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WeTest {
+
+
+    @Test
+    public void salve() {
+        assertEquals("a", "a");
+    }
+}
