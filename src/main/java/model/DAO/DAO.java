4d50ba424cd8643bd36b5ba063f4c3f7c0b16554
luigi-crescenzo7 <luigi.crescenzo.7@hotmail.it>
2022-01-31 11:15:43 +0100

    mockito fix

--- src/main/java/model/DAO/DAO.java
+++ src/main/java/model/DAO/DAO.java
@@ -5,45 +5,45 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface DAO<T> {
-   /**
-    * @param id rappresenta l'identificativo dell'entity
-    * @return null se non viene trovato nessun risultato,
-    * un'istanza di T nel caso in cui viene trovato un risultato
-    */
-   T getById(int id);
+    /**
+     * @param id rappresenta l'identificativo dell'entity
+     * @return null se non viene trovato nessun risultato,
+     * un'istanza di T nel caso in cui viene trovato un risultato
+     */
+    T getById(int id);
 
-   /**
-    * @return una lista di entity T
-    */
-   List<T> getAll();
+    /**
+     * @return una lista di entity T
+     */
+    List<T> getAll();
 
-   /**
-    * @param entity l'istanza da salvare
-    * @return false --> se l'operazione non va a buon fine,
-    * true --> se l'operazione va a buon fine
-    */
-   boolean save(T entity);
+    /**
+     * @param entity l'istanza da salvare
+     * @return false --> se l'operazione non va a buon fine,
+     * true --> se l'operazione va a buon fine
+     */
+    boolean save(T entity);
 
-   /**
-    * @param entity l'istanza da aggiornare
-    * @return false --> se l'operazione non va a buon fine,
-    * true --> se l'operazione va a buon fine
-    */
-   boolean update(T entity);
+    /**
+     * @param entity l'istanza da aggiornare
+     * @return false --> se l'operazione non va a buon fine,
+     * true --> se l'operazione va a buon fine
+     */
+    boolean update(T entity);
 
-   /**
-    * @param entity l'istanza da eliminare
-    * @return false --> se l'operazione non va a buon fine,
-    * true --> se l'operazione va a buon fine
-    */
-   boolean delete(T entity);
+    /**
+     * @param entity l'istanza da eliminare
+     * @return false --> se l'operazione non va a buon fine,
+     * true --> se l'operazione va a buon fine
+     */
+    boolean delete(T entity);
 
-   /**
-    * @param resultSet resultSet della query eseguita
-    * @param alias     eventuale alias del field
-    * @return l'istanza della della classe T popolata con le informazioni
-    * presenti nel resultSet
-    * @throws SQLException eccezione lanciata in caso di problemi
-    */
-   T extract(ResultSet resultSet, String alias) throws SQLException;
+    /**
+     * @param resultSet resultSet della query eseguita
+     * @param alias     eventuale alias del field
+     * @return l'istanza della della classe T popolata con le informazioni
+     * presenti nel resultSet
+     * @throws SQLException eccezione lanciata in caso di problemi
+     */
+    T extract(ResultSet resultSet, String alias) throws SQLException;
 }
