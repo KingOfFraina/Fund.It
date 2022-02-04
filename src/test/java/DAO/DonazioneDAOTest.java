60233ce8f7764b378677c611e5166eb8ff1f5579
luigi-crescenzo7 <luigi.crescenzo.7@hotmail.it>
2022-02-04 15:14:53 +0100

    refactor DonazioneDAOTest.java

--- src/test/java/DAO/DonazioneDAOTest.java
+++ src/test/java/DAO/DonazioneDAOTest.java
@@ -1,16 +1,14 @@
 package DAO;
 
-import model.DAO.DAO;
-import model.DAO.DonazioneDAO;
-import model.beans.Campagna;
-import model.beans.Donazione;
-import model.beans.Utente;
+import model.DAO.*;
+import model.beans.*;
 import model.storage.ConPool;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
@@ -19,14 +17,59 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DonazioneDAOTest {
 
     static DAO<Donazione> dao;
+    static DAO<Campagna> campagnaDAO;
+    static DAO<Utente> utenteDAO;
+    static DAO<Categoria> categoriaDAO;
     static Donazione d1;
-    static Donazione d3;
+    static Campagna campagna;
+    static Utente utente;
+    static Categoria categoria;
 
     @BeforeClass
     public static void setUp() {
         dao = new DonazioneDAO();
+        campagnaDAO = new CampagnaDAO();
+        utenteDAO = new UtenteDAO();
+        categoriaDAO = new CategoriaDAO();
+        campagna = new Campagna();
+        utente = new Utente();
         d1 = new Donazione();
-        d3 = new Donazione();
+        categoria = new Categoria();
+        categoria.setNome("cat");
+        categoriaDAO.save(categoria);
+
+        utente.setCf("We");
+        utente.setAdmin(true);
+        utente.setDataBan(LocalDateTime.now());
+        utente.setNome("strunz");
+        utente.setDataDiNascita(LocalDate.now());
+        utente.setCognome("lollo");
+        utente.setEmail("emailbella");
+        utente.setFotoProfilo("foto");
+        utente.setCap("cap");
+        utente.setCitta("city");
+        utente.setStrada("strada");
+        utente.setTelefono("sexo");
+        utente.setPassword("ldfoefer");
+        utenteDAO.save(utente);
+
+        campagna.setStato(StatoCampagna.ATTIVA);
+        campagna.setSommaRaccolta(100.0);
+        campagna.setSommaTarget(900.0);
+        campagna.setCategoria(categoria);
+        campagna.setTitolo("titolo");
+        campagna.setDescrizione("desc");
+        campagna.setUtente(utente);
+        campagnaDAO.save(campagna);
+
+        d1.setCampagna(campagna);
+        d1.setUtente(utente);
+        d1.setSommaDonata(90);
+        d1.setCommento("wella");
+        d1.setRicevuta("uhu");
+        d1.setDataOra(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES));
+        dao.save(d1);
+
     }
 
     @Test
@@ -38,22 +81,6 @@ public class DonazioneDAOTest {
 
     @Test
     public void testGetByIdGreaterThanZero() {
-        int id = 90;
-        d1.setIdDonazione(id);
-        d1.setAnonimo(true);
-        Utente utente = new Utente();
-        utente.setIdUtente(90);
-        d1.setUtente(utente);
-        d1.setAnonimo(true);
-        d1.setSommaDonata(90);
-        Campagna campagna = new Campagna();
-        campagna.setIdCampagna(90);
-        d1.setCampagna(campagna);
-        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
-        d1.setDataOra(now);
-        d1.setCommento("wewe");
-        d1.setRicevuta("ricevuta190");
-        assertTrue(dao.save(d1));
         Donazione d4 = dao.getById(d1.getIdDonazione());
         assertNotNull(d4);
         assertAll(() -> assertEquals(d1.getUtente().getIdUtente(), d4.getUtente().getIdUtente()),
@@ -63,7 +90,6 @@ public class DonazioneDAOTest {
                 () -> assertEquals(d1.getDataOra(), d4.getDataOra()),
                 () -> assertEquals(d1.getRicevuta(), d4.getRicevuta()),
                 () -> assertEquals(d1.isAnonimo(), d4.isAnonimo()));
-        assertTrue(dao.delete(d1));
     }
 
     @Test
@@ -107,18 +133,11 @@ public class DonazioneDAOTest {
     }
 
     @Test
-    public void testFalseUpdate() {
-        d1.setUtente(null);
-        d1.setCommento(null);
-        assertFalse(dao.update(d1));
+    public void testTrueUpdate() {
+        d1.setCommento("null");
+        assertTrue(dao.update(d1));
     }
 
-    @Test
-    public void testTrueUpdate() {
-        d3.setIdDonazione(20);
-        d3.setCommento("sesso");
-        assertTrue(dao.update(d3));
-    }
 
     @Test
     public void testGetByIdCampagna() {
@@ -126,16 +145,6 @@ public class DonazioneDAOTest {
         assertNotNull(donazioneDAO.getByIdCampagna(1));
     }
 
-    @Test
-    public void testThrowsRuntimeException() {
-        d1 = new Donazione();
-        Utente utente = new Utente();
-        utente.setIdUtente(999999);
-        d1.setIdDonazione(9999999);
-        d1.setUtente(utente);
-        assertThrows(RuntimeException.class,
-                () -> dao.save(d1));
-    }
 
     @Test
     public void testExtractDonazione() {
@@ -148,6 +157,9 @@ public class DonazioneDAOTest {
 
     @AfterClass
     public static void clearAll() {
+        categoriaDAO.delete(categoria);
+        utenteDAO.delete(utente);
+        campagnaDAO.delete(campagna);
         dao.delete(d1);
         ConPool.getInstance().closeDataSource();
     }
