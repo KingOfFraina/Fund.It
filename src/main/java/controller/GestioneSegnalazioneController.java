001caebced0c843e872663f9ab9ff021c9909938
Sabato Genovese <48244524+SabatoGenovese@users.noreply.github.com>
2022-02-08 20:29:42 +0100

    Fix

--- src/main/java/controller/GestioneSegnalazioneController.java
+++ src/main/java/controller/GestioneSegnalazioneController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import controller.utils.Validator;
-import model.DAO.UtenteDAO;
 import model.beans.Campagna;
 import model.beans.Segnalazione;
 import model.beans.StatoSegnalazione;
@@ -24,7 +23,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @WebServlet(name = "GestioneSegnalazioneController",
         value = "/segnalazioni/*")
