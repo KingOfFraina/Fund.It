5b273237528132e2dd90b70c557b6ce17400746d
Sabato Genovese <48244524+SabatoGenovese@users.noreply.github.com>
2022-02-08 20:23:04 +0100

    Continuo

--- src/test/java/Controller/utils/ValidatorTest.java
+++ src/test/java/Controller/utils/ValidatorTest.java
@@ -1,4 +1,39 @@
 package Controller.utils;
 
+import controller.utils.Validator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class ValidatorTest {
+   Validator validator;
+   HttpServletRequest request;
+
+   @Before
+   public void setup() {
+      request = Mockito.mock(HttpServletRequest.class);
+      validator = new Validator(request);
+   }
+
+   @Test
+   public void test1() {
+      String test = "test";
+      assertTrue(validator.isValidBean(String.class, test));
+   }
+
+   @Test
+   public void test2() {
+      assertFalse(validator.isValidBean(String.class, null));
+   }
+
+   @After
+   public void clean() {
+
+   }
 }
