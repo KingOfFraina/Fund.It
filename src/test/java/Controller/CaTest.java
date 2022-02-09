1a49e09a00f3494889ee07f891092db370a09085
Sabato Genovese <48244524+SabatoGenovese@users.noreply.github.com>
2022-02-09 12:57:48 +0100

    Fix

---
+++ src/test/java/Controller/CaTest.java
@@ -0,0 +1,211 @@
+package Controller;
+
+import controller.GestioneCampagnaController;
+import model.beans.Campagna;
+import model.beans.Utente;
+import model.services.CampagnaService;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+public class CaTest {
+    Campagna campagna;
+    Utente utente;
+    GestioneCampagnaController campagnaController;
+    HttpServletRequest mockRequest;
+    HttpServletResponse mockResponse;
+    HttpSession mockSession;
+    ServletContext mockContext;
+    CampagnaService mockService;
+    RequestDispatcher mockDispatcher;
+
+
+    @Before
+    public void setUp() {
+        mockRequest = Mockito.mock(HttpServletRequest.class);
+        mockResponse = Mockito.mock(HttpServletResponse.class);
+        mockSession = Mockito.mock(HttpSession.class);
+        mockContext = Mockito.mock(ServletContext.class);
+        mockDispatcher = Mockito.mock(RequestDispatcher.class);
+        mockService = Mockito.mock(CampagnaService.class);
+        campagnaController = new GestioneCampagnaController(mockService);
+        campagna = Mockito.mock(Campagna.class);
+        utente = Mockito.mock(Utente.class);
+    }
+
+    @Test
+    public void doPostTrueBranch() throws ServletException, IOException {
+        when(mockRequest.getSession()).thenReturn(mockSession);
+        when(mockSession.getAttribute("utente")).thenReturn(null);
+        when(mockRequest.getPathInfo()).thenReturn("/chiudiCampagna");
+        when(mockRequest.getServletContext()).thenReturn(mockContext);
+        when(mockContext.getContextPath()).thenReturn("/FundIt-1.0-SNAPSHOT");
+
+        campagnaController.doPost(mockRequest, mockResponse);
+
+        verify(mockRequest, atLeastOnce()).getPathInfo();
+        verify(mockResponse, atLeastOnce()).sendRedirect(anyString());
+    }
+
+    @Test
+    public void doPostFalseBranchDefault() throws ServletException, IOException {
+        when(mockRequest.getSession()).thenReturn(mockSession);
+        when(mockSession.getAttribute("utente")).thenReturn(new Utente());
+        when(mockRequest.getPathInfo()).thenReturn("/x");
+
+        campagnaController.doPost(mockRequest, mockResponse);
+
+        verify(mockRequest, atLeastOnce()).getPathInfo();
+        verify(mockResponse, atLeastOnce()).sendError(anyInt());
+    }
+
+    @Test
+    public void doPostFalseBranchChiudiCampagna1() throws ServletException, IOException {
+        when(mockRequest.getSession()).thenReturn(mockSession);
+        when(mockSession.getAttribute("utente")).thenReturn(new Utente());
+        when(mockRequest.getParameter("idCampagna")).thenReturn("1");
+        when(mockRequest.getPathInfo()).thenReturn("/chiudiCampagna");
+        when(mockService.trovaCampagna(anyInt())).thenReturn(new Campagna());
+        when(mockService.chiudiCampagna(any())).thenReturn(false);
+
+        campagnaController.doPost(mockRequest, mockResponse);
+
+        verify(mockRequest, atLeastOnce()).getPathInfo();
+        verify(mockResponse, atLeastOnce()).sendError(anyInt());
+    }
+
+    @Test
+    public void doPostFalseBranchChiudiCampagna2() throws ServletException, IOException {
+        when(mockRequest.getSession()).thenReturn(mockSession);
+        when(mockSession.getAttribute("utente")).thenReturn(new Utente());
+        when(mockRequest.getParameter("idCampagna")).thenReturn("1");
+        when(mockRequest.getPathInfo()).thenReturn("/chiudiCampagna");
+        when(mockService.trovaCampagna(anyInt())).thenReturn(new Campagna());
+        when(mockService.chiudiCampagna(any())).thenReturn(true);
+        when(mockRequest.getServletContext()).thenReturn(mockContext);
+        when(mockContext.getContextPath()).thenReturn("/FundIt-1.0-SNAPSHOT");
+
+        campagnaController.doPost(mockRequest, mockResponse);
+
+        verify(mockRequest, atLeastOnce()).getPathInfo();
+        verify(mockResponse, atLeastOnce()).sendRedirect(anyString());
+    }
+
+    @Test
+    public void doPostFalseBranchCancellaCampagna1() throws ServletException, IOException {
+        Utente utente = new Utente();
+        utente.setIdUtente(1);
+        when(mockRequest.getSession()).thenReturn(mockSession);
+        when(mockSession.getAttribute("utente")).thenReturn(utente);
+        when(mockRequest.getParameter("idCampagna")).thenReturn("1");
+        when(mockRequest.getPathInfo()).thenReturn("/cancellaCampagna");
+        when(mockService.trovaCampagna(anyInt())).thenReturn(new Campagna());
+        Utente utente2 = new Utente();
+        utente.setIdUtente(2);
+        Campagna campagna = new Campagna();
+        campagna.setUtente(utente2);
+        when(mockService.trovaCampagna(anyInt())).thenReturn(campagna);
+        campagnaController.doPost(mockRequest, mockResponse);
+
+        verify(mockRequest, atLeastOnce()).getPathInfo();
+        verify(mockResponse, atLeastOnce()).sendError(anyInt());
+    }
+
+    @Test
+    public void doPostFalseBranchCancellaCampagna2() throws ServletException, IOException {
+        Utente utente = new Utente();
+        utente.setIdUtente(1);
+        when(mockRequest.getSession()).thenReturn(mockSession);
+        when(mockSession.getAttribute("utente")).thenReturn(utente);
+        when(mockRequest.getParameter("idCampagna")).thenReturn("1");
+        when(mockRequest.getPathInfo()).thenReturn("/cancellaCampagna");
+        when(mockService.trovaCampagna(anyInt())).thenReturn(new Campagna());
+        Campagna campagna = new Campagna();
+        campagna.setUtente(utente);
+        when(mockService.trovaCampagna(anyInt())).thenReturn(campagna);
+        when(mockService.cancellaCampagna(any())).thenReturn(false);
+        campagnaController.doPost(mockRequest, mockResponse);
+
+        verify(mockRequest, atLeastOnce()).getPathInfo();
+        verify(mockService, atLeastOnce()).cancellaCampagna(any());
+        verify(mockService, atLeastOnce()).trovaCampagna(anyInt());
+    }
+
+    @Test
+    public void doPostFalseBranchCancellaCampagna3() throws ServletException, IOException {
+        Utente utente = new Utente();
+        utente.setIdUtente(1);
+        when(mockRequest.getSession()).thenReturn(mockSession);
+        when(mockSession.getAttribute("utente")).thenReturn(utente);
+        when(mockRequest.getParameter("idCampagna")).thenReturn("1");
+        when(mockRequest.getPathInfo()).thenReturn("/cancellaCampagna");
+        when(mockService.trovaCampagna(anyInt())).thenReturn(new Campagna());
+        Campagna campagna = new Campagna();
+        campagna.setUtente(utente);
+        when(mockService.trovaCampagna(anyInt())).thenReturn(campagna);
+        when(mockService.cancellaCampagna(any())).thenReturn(true);
+        when(mockService.rimborsaDonazioni(any(), any())).thenReturn(true);
+        campagnaController.doPost(mockRequest, mockResponse);
+
+        verify(mockRequest, atLeastOnce()).getPathInfo();
+        verify(mockService, atLeastOnce()).cancellaCampagna(any());
+        verify(mockService, atLeastOnce()).trovaCampagna(anyInt());
+        verify(mockService, atLeastOnce()).rimborsaDonazioni(any(), any());
+    }
+
+    @Test
+    public void doPostFalseBranchCancellaCampagna4() throws ServletException, IOException {
+        Utente utente = new Utente();
+        utente.setIdUtente(1);
+        when(mockRequest.getSession()).thenReturn(mockSession);
+        when(mockSession.getAttribute("utente")).thenReturn(utente);
+        when(mockRequest.getParameter("idCampagna")).thenReturn("1");
+        when(mockRequest.getPathInfo()).thenReturn("/cancellaCampagna");
+        when(mockService.trovaCampagna(anyInt())).thenReturn(new Campagna());
+        Campagna campagna = new Campagna();
+        campagna.setUtente(utente);
+        when(mockService.trovaCampagna(anyInt())).thenReturn(campagna);
+        when(mockService.cancellaCampagna(any())).thenReturn(true);
+        when(mockService.rimborsaDonazioni(any(), any())).thenReturn(false);
+        campagnaController.doPost(mockRequest, mockResponse);
+
+        verify(mockRequest, atLeastOnce()).getPathInfo();
+        verify(mockService, atLeastOnce()).cancellaCampagna(any());
+        verify(mockService, atLeastOnce()).trovaCampagna(anyInt());
+        verify(mockService, atLeastOnce()).rimborsaDonazioni(any(), any());
+    }
+
+    @Test
+    public void doPostFalseBranchCreaCampagna1() throws ServletException, IOException {
+        Utente utente = new Utente();
+        utente.setIdUtente(1);
+        when(mockRequest.getSession()).thenReturn(mockSession);
+        when(mockSession.getAttribute("utente")).thenReturn(utente);
+        when(mockRequest.getParameter("idCampagna")).thenReturn("1");
+        when(mockRequest.getPathInfo()).thenReturn("/creaCampagna");
+        when(mockService.trovaCampagna(anyInt())).thenReturn(new Campagna());
+        Campagna campagna = new Campagna();
+        campagna.setUtente(utente);
+        when(mockService.trovaCampagna(anyInt())).thenReturn(campagna);
+        when(mockService.cancellaCampagna(any())).thenReturn(true);
+        when(mockService.rimborsaDonazioni(any(), any())).thenReturn(false);
+        campagnaController.doPost(mockRequest, mockResponse);
+
+        verify(mockRequest, atLeastOnce()).getPathInfo();
+        verify(mockService, atLeastOnce()).cancellaCampagna(any());
+        verify(mockService, atLeastOnce()).trovaCampagna(anyInt());
+        verify(mockService, atLeastOnce()).rimborsaDonazioni(any(), any());
+    }
+}
---
+++ src/test/java/Controller/utils/FileServletTest.java
@@ -0,0 +1,42 @@
+package Controller.utils;
+
+import controller.utils.FileServlet;
+import org.junit.Before;
+import org.junit.Test;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import static org.mockito.Mockito.*;
+
+public class FileServletTest {
+   HttpServletRequest request;
+   HttpServletResponse response;
+   FileServlet fileServlet;
+
+   @Before
+   public void setup() {
+      request = mock(HttpServletRequest.class);
+      response = mock(HttpServletResponse.class);
+      fileServlet = new FileServlet();
+   }
+
+   @Test
+   public void testFileTrovato() throws IOException {
+      //Nota inserire il file test.jpg nella cartella
+      when(request.getPathInfo()).thenReturn("/test.jpg");
+
+      fileServlet.doGet(request, response);
+      verify(request, atLeastOnce()).getPathInfo();
+      verify(request, atLeastOnce()).getHeader(anyString());
+      verify(request, atLeastOnce()).getDateHeader(anyString());
+   }
+
+   @Test
+   public void testFileNonTrovato() throws IOException {
+      when(request.getPathInfo()).thenReturn("/test1.jpg");
+
+      fileServlet.doGet(request, response);
+      verify(request, atLeastOnce()).getPathInfo();
+      verify(response, atLeastOnce()).sendError(anyInt());
+   }
+}
